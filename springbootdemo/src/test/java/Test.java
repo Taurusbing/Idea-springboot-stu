a9350d0f58f146907fd3e4fa59bbfcc27ca88323
Taurusbing <760494282@qq.com>
2019-01-24 11:12:46 +0800

    本地修改004

--- springbootdemo/src/test/java/Test.java
+++ springbootdemo/src/test/java/Test.java
@@ -6,6 +6,7 @@ public class Test {
         System.out.println("本地端修改");
         System.out.println("主分支远程修改02");
         System.out.println("主分支远程修改01");
-        System.out.println("主分支远程修改003");
+        System.out.println("主分支本地修改003");
+        System.out.println("主分支本地修改004");
     }
 }
