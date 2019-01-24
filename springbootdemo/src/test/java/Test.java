61b328fb8a5e0f1746eb4fbb59865a336d17bebb
Taurusbing <38279369+Taurusbing@users.noreply.github.com>
2019-01-24 09:46:13 +0800

    主分支远程修改01
    主分支远程修改01

--- springbootdemo/src/test/java/Test.java
+++ springbootdemo/src/test/java/Test.java
@@ -4,6 +4,6 @@ public class Test {
         System.out.println("测试合并");
         System.out.println("测试为push主分支是否能看到");
         System.out.println("本地端修改");
-        System.out.println("主分支远程修改");
+        System.out.println("主分支远程修改01");
     }
 }
