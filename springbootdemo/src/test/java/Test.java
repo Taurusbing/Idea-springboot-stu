ba8002a33cd2c4b1b910d058ec77964449e783dd
Taurusbing <38279369+Taurusbing@users.noreply.github.com>
2019-01-24 11:09:07 +0800

    主分支远程修改004
    主分支远程修改004

--- springbootdemo/src/test/java/Test.java
+++ springbootdemo/src/test/java/Test.java
@@ -7,5 +7,6 @@ public class Test {
         System.out.println("主分支远程修改02");
         System.out.println("主分支远程修改01");
         System.out.println("主分支远程修改003");
+        System.out.println("主分支远程修改004");
     }
 }
