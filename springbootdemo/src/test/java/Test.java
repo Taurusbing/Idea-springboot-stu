6901bca3f5b15b75f7c389eb1ccbdcd3cc619d16
Taurusbing <760494282@qq.com>
2019-02-18 13:50:15 +0800

    1234

--- springbootdemo/src/test/java/Test.java
+++ springbootdemo/src/test/java/Test.java
@@ -1,3 +1,6 @@
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Test {
     public static void main(String[] args) {
         System.out.println("测试分支");
@@ -5,5 +8,15 @@ public class Test {
         System.out.println("测试为push主分支是否能看到");
         System.out.println("本地端修改");
         System.out.println("本地修改2");
+        System.out.println("主分支远程修改02");
+        System.out.println("主分支远程修改01");
+        System.out.println("主分支本地修改003");
+        System.out.println("主分支本地修改004");
+        System.out.println("主分支远程修改003");
+        System.out.println("主分支远程修改004");
+        System.out.println("主分支远程修改005");
+
+        System.out.println("1234");
+        System.out.println(new SimpleDateFormat("yyyy-MM-dd").format(new Date()));
     }
 }
